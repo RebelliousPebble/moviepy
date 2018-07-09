d57212bb58c24f2da618b76b4168f54f98a886b0
RebelliousPebble <grk2001@talktalk.net>
2018-07-09 10:33:55 +0100

    Update __init__.py

--- moviepy/audio/fx/all/__init__.py
+++ moviepy/audio/fx/all/__init__.py
@@ -11,5 +11,12 @@ import moviepy.audio.fx as fx
 __all__ = [name for _, name, _ in pkgutil.iter_modules(
     fx.__path__) if name != "all"]
 
-for name in __all__:
-    exec("from ..%s import %s" % (name, name))
+#for name in __all__:
+    #exec("from ..%s import %s" % (name, name))
+
+import __moviepy.audio.fx.audio_fadein as audio_fadein
+import __moviepy.audio.fx.audio_fadeout as audio_fadeout
+import __moviepy.audio.fx.audio_left_right as audio_left_right
+import __moviepy.audio.fx.audio_loop as audio_loop
+import __moviepy.audio.fx.audio_normalize as audio_normalize
+import __moviepy.audio.fx.volumex as volumex
